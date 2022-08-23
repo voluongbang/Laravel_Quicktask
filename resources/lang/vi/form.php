212073051bc0ad45fff776aafcff6d48d1016de7
Bang.Voluong <vo.luong.bang@sun-asterisk.com>
2022-08-23 17:02:44 +0700

    Finish chapter 9

---
+++ resources/lang/vi/form.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'email' => 'Địa chỉ email',
+    'enter_email' => 'Nhập địa chỉ email',
+    'enter_password' => 'Nhập mật khẩu',
+    'password' => 'Mật khẩu',
+    'first_name' => 'Họ',
+    'last_name' => 'Tên',
+    'username' => 'Mã người dùng',
+    'not_show_mail' => 'Chúng tôi sẽ không hiển thị mail của bạn ở bất cứ đâu',
+    'remember' => 'Ghi nhớ đăng nhập',
+    'confirmPassword' => 'Nhập lại mật khẩu',
+    'login' => 'Đăng nhập',
+    'logout' => 'Đăng xuất',
+    'signup' => 'Đăng ký',
+    'forgotPassword' => 'Quên mật khẩu?'
+];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LanguageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,3 +22,5 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('language/{lang}', [LanguageController::class, 'changeLanguage'])->name('language');
